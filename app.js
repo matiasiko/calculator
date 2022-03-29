127f9392aa637f91d40aa8b2ea08ec574330b9db
matiasiko <alcatel1962@outlook.com>
2022-03-29 11:35:10 +0200

    add numbers to display

--- app.js
+++ app.js
@@ -3,19 +3,24 @@ const keyBtns = document.querySelectorAll('button');
 const clear = document.getElementById('clear');
 
 function displayValue(number) {
-    console.log(number)
+    // if current display value is 0 , replace it, if not add number
+    const displayValue = display.textContent;
+    display.textContent = displayValue === '0' ? number : displayValue + number;
 }
 
 // add eventlisteners for numbers, operators and decimal buttons
 keyBtns.forEach((keyBtn) => {
     if (keyBtn.classList.length === 0) {
-      keyBtn.addEventListener('click', (e) => displayValue(e.target.value));
+      keyBtn.addEventListener('click', () => displayValue(keyBtn.value));
     } else if (keyBtn.classList.contains('operator')) {
       keyBtn.addEventListener('click', () => displayValue(keyBtn.value));
     } else if (keyBtn.classList.contains('decimal')) {
-      keyBtn.addEventListener('click', () => displayValue());
+      keyBtn.addEventListener('click', () => displayValue(keyBtn.value));
     }
   });
+
+// reset display
+
 
 
 
