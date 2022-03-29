39c4bcbacd1c477d38e56e6195cdcf78ee5374ef
matiasiko <alcatel1962@outlook.com>
2022-03-29 12:46:47 +0200

    prevent adding multiply dots

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const display = document.getElementById('result');
 const keyBtns = document.querySelectorAll('button');
 const clear = document.getElementById('clear');
+const equal = document.getElementById('equal');
 
 function displayValue(number) {
     // if current display value is 0 , replace it, if not add number
@@ -15,13 +16,46 @@ keyBtns.forEach((keyBtn) => {
     } else if (keyBtn.classList.contains('operator')) {
       keyBtn.addEventListener('click', () => displayValue(keyBtn.value));
     } else if (keyBtn.classList.contains('decimal')) {
-      keyBtn.addEventListener('click', () => displayValue(keyBtn.value));
+      keyBtn.addEventListener('click', () => addDecimal());
     }
   });
 
+  function addDecimal() {
+    // If no decimal, add one
+    if (!display.textContent.includes('.')) {
+      display.textContent = `${display.textContent}.`;
+    }
+  }
+
 // reset display
 
+function add(a, b) {
+  return a + b;
+}
 
+function subtract(a, b) {
+  return a - b;
+}
 
+function multiply(a, b) {
+  return a * b;
+}
 
+function divide(a, b) {
+  return a / b;
+}
 
+function operate(num1, num2, operator) {
+  switch (operator) {
+      case "+":
+          return add(num1, num2);
+      case "-":
+          return subtract(num1, num2);
+      case "*":
+          return multiply(num1, num2);
+      case "÷":
+          return divide(num1, num2);
+  }
+};
+
+// equal.addEventListener('click', calculate);
