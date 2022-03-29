2ca1e67fb9208793810d1cf9424673995a697ca2
matiasiko <alcatel1962@outlook.com>
2022-03-29 10:45:58 +0200

    removed ul from html

---
+++ app.js
@@ -0,0 +1,38 @@
+
+
+
+
+
+
+
+
+
+function add(a, b) {
+    return a + b;
+}
+
+function subtract(a, b) {
+    return a - b;
+}
+
+function multiply(a, b) {
+    return a * b;
+}
+
+function divide(a, b) {
+    return a / b;
+}
+
+function operate(num1, num2, operator) {
+    switch (operator) {
+        case "+":
+            return add(num1, num2);
+        case "-":
+            return subtract(num1, num2);
+        case "*":
+            return multiply(num1, num2);
+        case "÷":
+            return divide(num1, num2);
+    }
+};
+
